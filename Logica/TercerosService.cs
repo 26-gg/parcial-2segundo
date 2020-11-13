d0c768bd89fc0b29fa66ac7df39d521eb6a91b0c
26-gg <guiller.nike26@gmail.com>
2020-11-13 01:57:20 -0500

    Segundo Commit

---
+++ Logica/TercerosService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Datos;
+using Entity;
+
+namespace Logica
+{
+    public class TercerosService
+    {
+        private readonly EntidadContext _context;
+
+        public TercerosService(EntidadContext context)
+        {
+            _context = context;
+        }
+
+        public class ConsultarTerceroResponse
+        {
+            public ConsultarTerceroResponse(Tercero tercero)
+            {
+                Error = false;
+                Tercero = tercero;
+            }
+            public ConsultarTerceroResponse(string mensaje)
+            {
+                Error = true;
+                Mensaje = mensaje;
+            }
+
+            public bool Error { get; set; }
+            public string Mensaje { get; set; }
+            public Tercero Tercero { get; set; }
+        }
+
+        public ConsultarTerceroResponse BuscarTercero(string identificacion)
+        {
+            try
+            {
+                var tercero = _context.Terceros.Where(c=>c.NumIdentificacion == identificacion).FirstOrDefault();
+                if (tercero == null)
+                {
+                    return new ConsultarTerceroResponse("El tercero no se encuentra registrado");
+                }
+                return new ConsultarTerceroResponse(tercero);
+            }
+            catch (Exception e)
+            {
+                
+                return new ConsultarTerceroResponse("Error en la Aplicacion: " + e.Message);
+            }
+            
+        }
+    }
+}
--- segundoParcial/Startup.cs
+++ segundoParcial/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Datos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace segundoParcial
 {
@@ -20,7 +24,32 @@ namespace segundoParcial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<EntidadContext>(p=>p.UseSqlServer(connectionString));
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "School API",
+                    Description = "School API - ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://cla.dotnetfoundation.org/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Unicesar",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/borisgr04/CrudNgDotNetCore3"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licencia dotnet foundation",
+                        Url = new Uri("https://www.byasystems.co/license"),
+                    }
+                });
+            });
+            
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
@@ -56,6 +85,12 @@ namespace segundoParcial
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
             app.UseSpa(spa =>
